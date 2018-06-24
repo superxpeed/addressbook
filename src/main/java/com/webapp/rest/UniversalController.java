def04dba89d220db0bc39937e3b9bc825c006df8
komesergey <komesergey@gmail.com>
2018-06-25 01:11:00 +0300

    Started adding snapshot functionality

--- src/main/java/com/webapp/rest/UniversalController.java
+++ src/main/java/com/webapp/rest/UniversalController.java
@@ -16,23 +16,31 @@ import java.util.List;
 @Controller
 public class UniversalController{
 
-        @ResponseBody
-        @RequestMapping("/getList4UniversalListForm")
-        public PageDataDto<TableDataDto> getList (@RequestParam(value = "start") int start,
-                                                  @RequestParam(value = "pageSize") int pageSize,
-                                                  @RequestParam(value = "sortName") String sortName,
-                                                  @RequestParam(value = "sortOrder") String sortOrder,
-                                                  @RequestParam(value = "cache") String cache,
-                                                  @RequestBody List<FilterDto> filterDto) {
+    @ResponseBody
+    @RequestMapping("/getList4UniversalListForm")
+    public PageDataDto<TableDataDto> getList (@RequestParam(value = "start") int start,
+                                              @RequestParam(value = "pageSize") int pageSize,
+                                              @RequestParam(value = "sortName") String sortName,
+                                              @RequestParam(value = "sortOrder") String sortOrder,
+                                              @RequestParam(value = "cache") String cache,
+                                              @RequestBody List<FilterDto> filterDto) {
 
-            System.out.println(filterDto);
+        System.out.println(filterDto);
 
-            TableDataDto td = new TableDataDto<>(GridUtils.selectCachePage(start,pageSize, sortName, sortOrder, filterDto, cache), GridUtils.getTotalDataSize(cache, filterDto));
-            PageDataDto<TableDataDto> dto = new PageDataDto<>();
-            dto.setData(td);
-            dto.setTitle("Lol kek");
-            dto.setBreadcrumbs(new ArrayList<>());
-            dto.setFieldDescriptionMap(UniversalFieldsDescriptor.getFieldDescriptionMap(cache));
-            return dto;
-        }
+        TableDataDto td = new TableDataDto<>(GridUtils.selectCachePage(start,pageSize, sortName, sortOrder, filterDto, cache), GridUtils.getTotalDataSize(cache, filterDto));
+        PageDataDto<TableDataDto> dto = new PageDataDto<>();
+        dto.setData(td);
+        dto.setTitle("Lol kek");
+        dto.setBreadcrumbs(new ArrayList<>());
+        dto.setFieldDescriptionMap(UniversalFieldsDescriptor.getFieldDescriptionMap(cache));
+        return dto;
+    }
+
+    @ResponseBody
+    @RequestMapping("/takeFullSnapshot")
+    public PageDataDto<Float> takeFullSnapshot(){
+        PageDataDto<Float> dto = new PageDataDto<>();
+        dto.setData(GridUtils.takeFullSnapshot());
+        return dto;
+    }
 }
