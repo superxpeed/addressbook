3c5dd4a36eb11696fbabbea05894925c4ecb491b
Nikita Schneider <komesergey@gmail.com>
2023-02-14 02:52:09 +0300

    Refactoring

--- eureka-server/src/main/kotlin/com/addressbook/eureka/EurekaApplication.kt
+++ eureka-server/src/main/kotlin/com/addressbook/eureka/EurekaApplication.kt
@@ -1,4 +1,4 @@
-package com.addressbook.eureka;
+package com.addressbook.eureka
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration
